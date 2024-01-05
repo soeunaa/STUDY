d8ba43a61647fd0e5ec6ea1392a4aad10b99eecc
SoEun <psoeun218@gmail.com>
2024-01-05 14:09:42 +0900

    Ch 3-3 문제 2번.c 풀이 및 업로드

---
+++ 열혈 C 프로그래밍 /Ch 03 변수와 연산자/Ch 3-3 문제 2번.c
@@ -0,0 +1,25 @@
+/*
+문제 2. 프로그램 사용자로부터 세 개의 정수 num1, num2, num3을 순서대로 입력 받은 후
+       다음 연산의 결과를 출력하는 프로그램을 작성해보자.
+
+       num1 x num2 + num3
+
+       단, 입력 받은 세 개의 정수가 2, 4, 6이라면 다음의 형태로 출력을 해야 한다
+       2 x 4 + 6 = 14
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+  int num1, num2, num3;
+  int result
+  
+  printf("세 개의 정수 입력: ");
+  scanf("%d %d %d", &num1, &num2, &num3);
+  result = num1 * num2 + num3;
+
+  printf("%d x %d + %d = %d \n", num1, num2, num3, result);
+  return 0;
+}
