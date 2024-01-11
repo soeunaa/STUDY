683f128832848439ee843101f5dc0e28d844a6b0
SoEun <psoeun218@gmail.com>
2024-01-11 15:27:15 +0900

    내용 추가

--- 열혈 C 프로그래밍 /Ch 05 상수와 기본 자료형/Ch 5-2 예제 1번.c
+++ 열혈 C 프로그래밍 /Ch 05 상수와 기본 자료형/Ch 5-2 예제 1번.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(void)
+{
+  char ch1 = 'A', ch2 = 65;
+  char ch3 = 'Z', ch4 = 90;
+
+  printf("%c %d \n", ch1, ch1);
+  printf("%c %d \n", ch2, ch2);
+  printf("%c %d \n", ch3, ch3);
+  printf("%c %d \n", ch4, ch4);
+  // %c : 문자의 형태로 데이터를 입력 또는 출력하라
+  return 0;
+}
